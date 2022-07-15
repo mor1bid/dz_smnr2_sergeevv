17231a5e913b9fd1e4d9565bd686033277e819e5
De <spark49942@gmail.com>
2022-07-16 01:36:02 +0300

    Zadacha131

--- Program.cs
+++ Program.cs
@@ -1,13 +1,17 @@
 ﻿int index=999, count=0;
 while (count<=index) {
     Console.WriteLine("Введите число: ");
-    int numa = Convert.ToInt32(Console.ReadLine()), last = numa % 10; 
+    int numa = Convert.ToInt32(Console.ReadLine()), lasta = numa % 10, lastb = numa % 100; 
     if (numa > 100) 
     {
-        Console.WriteLine(last); }
+        Console.WriteLine(lasta); }
+        else if (numa > 1000) 
+        {
+            Console.WriteLine(lastb);
+        }
         else 
         {
-        Console.WriteLine("третьей цифры нет");
+            Console.WriteLine("третьей цифры нет");
         }
         count++;
     }
