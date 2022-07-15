8009b8c580c0c51c8a35751278906c4ca0332f1c
De <spark49942@gmail.com>
2022-07-16 01:21:48 +0300

    Zadacha131

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿void Zadacha10()
+{
 Console.WriteLine("Hello, World!");
+}
+
+void Zadacha13()
+{
+int index=999, count=0;
+while (count<=index) {
+    Console.WriteLine("Введите число: ");
+    int numa = Convert.ToInt32(Console.ReadLine()), last = numa % 10; 
+    if (numa > 100) 
+    {
+        Console.WriteLine(last); }
+        else 
+        {
+        Console.WriteLine("третьей цифры нет");
+        }
+        count++;
+    }
+}
+
+void Zadacha15()
+{
+Console.WriteLine("Hello, World!");
+}
