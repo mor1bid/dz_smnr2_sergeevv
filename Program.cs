6daeba098e48973228857e6b241ae7be116ea2a8
De <spark49942@gmail.com>
2022-07-16 02:43:36 +0300

    Zadacha133

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ while (count<=index) {
         lastb = numa % 100 / 10; 
     if (numa > 10000) 
     {
-        lastb = (numa % 10000 / 1000) / 10;
+        lastb = (numa % 10000 / 1000);
         Console.WriteLine(lastb); }
         else if (numa > 1000)
         {
