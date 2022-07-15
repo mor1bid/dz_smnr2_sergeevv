7b402ab7f5e99b2f6870cb25ed42bab88a8180cb
De <spark49942@gmail.com>
2022-07-16 02:15:55 +0300

    Zadacha133

--- Program.cs
+++ Program.cs
@@ -1,13 +1,17 @@
 ﻿int index=999, count=0;
 while (count<=index) {
     Console.WriteLine("Введите число: ");
-    int numa = Convert.ToInt32(Console.ReadLine()), 
+    ulong numa = Convert.ToUInt32(Console.ReadLine()), 
         lasta = numa % 10, 
         lastb = numa % 100 / 10; 
-    if (numa > 1000) 
+    if (numa > 10000) 
     {
-        lastb = (numa % 1000 / 100) % 10;
+        lastb = (numa % 100000 / 10000) % 10;
         Console.WriteLine(lastb); }
+        else if (numa > 1000)
+        {
+            Console.WriteLine(lastb);
+        }
         else if (numa > 100) 
         {
             Console.WriteLine(lasta);
