ff6a9f3ce8c1a6e7023a15e8cf1222fb5cb46dac
De <spark49942@gmail.com>
2022-07-17 01:49:36 +0300

    DOZ2-1

--- Program.cs
+++ Program.cs
@@ -1,14 +1,15 @@
 ﻿int index=999, count=0;
 while (count<=index) 
 {
-    Console.WriteLine("Введите число: ");
-    int num = Convert.ToInt32(Console.ReadLine()), 
-        two = num % 100 / 10;
-        if (num<1000 && num>99) 
-        {
-        Console.WriteLine(two);
-        } 
-        else 
-        Console.WriteLine("Число " + num + " не является трёхзначным!");
+    Console.WriteLine("Введите дату рождения: ");
+    int myday = Convert.ToInt32(Console.ReadLine()), 
+        mymonth = Convert.ToInt32(Console.ReadLine()),
+        myyear = Convert.ToInt32(Console.ReadLine()),
+        day = 1, month = 7, year = 2022, old=0;
+    while (myyear <= year) 
+    {
+    old++; 
+    }
+    Console.WriteLine("Вам " + old);
 count++;
 } 
