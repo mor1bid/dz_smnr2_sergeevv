a56cddea1c4f51811524750b9af3ceab9070bd92
De <spark49942@gmail.com>
2022-07-17 01:51:49 +0300

    DOZ2-1

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
 ﻿int index=999, count=0;
 while (count<=index) 
 {
+    int year = 2022, old=0;
     Console.WriteLine("Введите дату рождения: ");
     int myday = Convert.ToInt32(Console.ReadLine()), 
         mymonth = Convert.ToInt32(Console.ReadLine()),
         myyear = Convert.ToInt32(Console.ReadLine()),
-        day = 1, month = 7, year = 2022, old=0;
     while (myyear <= year) 
     {
     old++; 
