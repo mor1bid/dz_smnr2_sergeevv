7c2fce862557e0d9fdbbb3c4d909a96848eda9e0
De <spark49942@gmail.com>
2022-07-16 04:35:37 +0300

    Zadacha101

--- Program.cs
+++ Program.cs
@@ -2,8 +2,13 @@
 while (count<=index) 
 {
     Console.WriteLine("Введите число: ");
-    int numa = Convert.ToInt32(Console.ReadLine()), 
-        two = numa % 100 / 10;
+    int num = Convert.ToInt32(Console.ReadLine()), 
+        two = num % 100 / 10;
+        if (num>1000 && num<99) 
+        {
         Console.WriteLine(two);
+        } 
+        else 
+        Console.WriteLine("Число " + num + " не является трёхзначным!");
 count++;
 } 
