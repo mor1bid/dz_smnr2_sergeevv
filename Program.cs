c5ea4c6bf41d50c64988ccd35af0736fd69c4770
De <spark49942@gmail.com>
2022-07-17 02:07:24 +0300

    DOZ2-1

--- Program.cs
+++ Program.cs
@@ -6,11 +6,10 @@ while (count<=index)
     int //myday = Convert.ToInt32(Console.ReadLine()), 
         //mymonth = Convert.ToInt32(Console.ReadLine()),
         myyear = Convert.ToInt32(Console.ReadLine());
-    while (myyear < year) 
+    while (myyear <= year) 
     {
-    myyear++; 
-    }
-    myyear %= 100;
+    myyear = (myyear%100)-(year%100);
     Console.WriteLine("Вам " + myyear);
+    }
 count++;
 } 
