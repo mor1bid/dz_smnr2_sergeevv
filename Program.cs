3d1f436e7fdf99e94c0c0eabd9bccc336f02fc6a
De <spark49942@gmail.com>
2022-07-16 03:27:28 +0300

    Zadacha135

--- Program.cs
+++ Program.cs
@@ -4,18 +4,23 @@ while (count<=index)
     Console.WriteLine("Введите число: ");
     ulong numa = Convert.ToUInt64(Console.ReadLine()), 
         lasta = numa % 10, 
-        lastb = numa % 100 / 10;
+        lastb = numa % 100 / 10,
+        ten = 10;
         while (numa>=100)
     {
         if (numa > 1000)
         {
             Console.WriteLine(lastb);
+            break;
         }
         else 
         {
-            lastb %= 10;
+            lastb %= ten;
             Console.WriteLine(lastb);
+            ten++;
+            //break;
         }
+        Console.WriteLine("третьего числа нет");
         count++;
     }
 }
