246f9e3030dd14266011b821a3fab82a84572cf7
De <spark49942@gmail.com>
2022-07-16 04:37:02 +0300

    Zadacha101 перепутал стрелки

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ while (count<=index)
     Console.WriteLine("Введите число: ");
     int num = Convert.ToInt32(Console.ReadLine()), 
         two = num % 100 / 10;
-        if (num>1000 && num<99) 
+        if (num<1000 && num>99) 
         {
         Console.WriteLine(two);
         } 
