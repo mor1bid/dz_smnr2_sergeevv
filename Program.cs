7f385019e833b67188e696cc70ea0e84c24d68c7
De <spark49942@gmail.com>
2022-07-16 05:13:44 +0300

    master

--- Program.cs
+++ Program.cs
@@ -1,26 +1,3 @@
-﻿void Zadacha10()
-{
-Console.WriteLine("Hello, World!");
-}
-
-void Zadacha13()
-{
-int index=999, count=0;
-while (count<=index) {
-    Console.WriteLine("Введите число: ");
-    int numa = Convert.ToInt32(Console.ReadLine()), last = numa % 10; 
-    if (numa > 100) 
-    {
-        Console.WriteLine(last); }
-        else 
-        {
-        Console.WriteLine("третьей цифры нет");
-        }
-        count++;
-    }
-}
-
-void Zadacha15()
-{
-Console.WriteLine("Hello, World!");
-}
+﻿//git switch Zadacha10
+//git switch Zadacha13
+//git switch Zadacha15
