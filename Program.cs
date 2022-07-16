53a72144ca061c48692a31584349fe29f3b05971
De <spark49942@gmail.com>
2022-07-17 02:22:46 +0300

    DOZ2-3

--- Program.cs
+++ Program.cs
@@ -1,12 +1,33 @@
 ﻿int index=999, count=0;
 while (count<=index) 
 {
-    int year = 2022, old=0;
+    int day = 1, month = 7, year = 2022;
     Console.WriteLine("Введите дату рождения: ");
-    int //myday = Convert.ToInt32(Console.ReadLine()), 
-        //mymonth = Convert.ToInt32(Console.ReadLine()),
+    int myday = Convert.ToInt32(Console.ReadLine()), 
+        mymonth = Convert.ToInt32(Console.ReadLine()),
         myyear = Convert.ToInt32(Console.ReadLine());
-    myyear = myyear-year;
-    Console.WriteLine("Вам " + myyear);
+    if (mymonth <= 31) 
+    {
+    myday = day-myday;
+    Console.WriteLine("Вам " + myday + " дней, ");
+    }
+    else
+    Console.WriteLine("Введена некорректная дата");
+
+    if (mymonth <= 12) 
+    {
+    mymonth = month-mymonth;
+    Console.WriteLine("Вам " + mymonth + " месяцев, ");
+    }
+    else
+    Console.WriteLine("Введена некорректная дата");
+    
+    if (myyear <= year) 
+    {
+    myyear = year-myyear;
+    Console.WriteLine(myyear + " года.");
+    }
+    else
+    Console.WriteLine("Введена некорректная дата");
 count++;
 } 
