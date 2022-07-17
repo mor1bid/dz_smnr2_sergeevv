82a600944c4235c4b67434c99ee68a6b273ac4bf
De <spark49942@gmail.com>
2022-07-17 04:24:39 +0300

    DOZ3-1tf

--- Program.cs
+++ Program.cs
@@ -1,14 +1,15 @@
 ﻿int index=999, count=0;
 while (count<=index) 
 {
-    double money = 1000, grow = 0.015, moneygrow = money * grow, month=0;
+    double money = 1000, grow = 1.5, month=0;
+    //double moneygrow = money * grow;
     Console.WriteLine("Введите кол-во месяцев: ");
     double mymonth = Convert.ToInt32(Console.ReadLine());
     while (month<=mymonth) 
     {
         if (month % 2 == 0) 
         {
-            //double moneygrow = money * grow;
+            double moneygrow = money * grow / 100;
             money += moneygrow;
         }
         month++;
