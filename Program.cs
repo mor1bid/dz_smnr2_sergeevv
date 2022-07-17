5ae5e5d28f794b62a5c8288fbb28a5218727ebbb
De <spark49942@gmail.com>
2022-07-17 04:29:13 +0300

    DOZ3-1tf комментарий

--- Program.cs
+++ Program.cs
@@ -2,14 +2,13 @@
 while (count<=index) 
 {
     double money = 1000, grow = 1.5, month=0;
-    //double moneygrow = money * grow;
     Console.WriteLine("Введите кол-во месяцев: ");
     double mymonth = Convert.ToInt32(Console.ReadLine());
     while (month<=mymonth) 
     {
         if (month % 2 == 0) 
         {
-            double moneygrow = money * grow / 100;
+            double moneygrow = money * grow / 100; //Чтобы получать полный ответ
             money += moneygrow;
         }
         month++;
